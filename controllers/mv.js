7ac182da5f57354f078ecb66724799fa1530b76c
bzxnaga <bzxnaga@gmail.com>
2020-04-21 00:40:07 +0700

    crud done

--- controllers/mv.js
+++ controllers/mv.js
@@ -19,6 +19,110 @@ class Controller{
         })
     }
 
+    static createMovie(req,res){
+        res.render('mvadd')
+    }
+
+    static addMovie(req,res){
+        const body = req.body
+        let phid ;
+        if(req.body.ph == 'Walt Disney Studios'){
+            phid = 1
+        }
+        if(req.body.ph == 'Pixar'){
+            phid = 2
+        }
+        if(req.body.ph == 'Warner Bros'){
+            phid = 3
+        }
+        if(req.body.ph == 'Universal Pictures'){
+            phid = 4
+        }
+        if(req.body.ph == 'Paramount Picture'){
+            phid = 5
+        }
+
+        body.name = body.name
+        body.released_year = body.released_year
+        body.genre = body.genre
+        body.createdAt = new Date()
+        body.updatedAt = new Date()
+        body.productionHouseId = phid
+
+        Movie.create(body)
+        .then(()=>{
+            res.redirect('/mv')
+        })
+        .catch(err=>{
+            res.send(err)
+        })
+
+    }
+
+    static editForm (req,res){
+        const id = req.params.id
+        Movie.findByPk(id, { include: { model: ProductionHouse } })
+          .then(data => {
+            res.render('mvedit', { data })
+            // res.send(data)
+            // console.log(data.ProductionHouse.name_prodHouse)
+          })
+          .catch(err => {
+            res.send(err)
+          })
+    }
+
+    static changeMovie(req,res){
+        const body = req.body
+        let phid ;
+        if(req.body.ph == 'Walt Disney Studios'){
+            phid = 1
+        }
+        if(req.body.ph == 'Pixar'){
+            phid = 2
+        }
+        if(req.body.ph == 'Warner Bros'){
+            phid = 3
+        }
+        if(req.body.ph == 'Universal Pictures'){
+            phid = 4
+        }
+        if(req.body.ph == 'Paramount Picture'){
+            phid = 5
+        }
+
+        body.name = body.name
+        body.released_year = body.released_year
+        body.genre = body.genre
+        body.createdAt = new Date()
+        body.updatedAt = new Date()
+        body.productionHouseId = phid
+
+        Movie.update(body,{ where: { id: req.params.id } })
+        .then(()=>{
+            res.redirect('/mv')
+        })
+        .catch(err=>{
+            res.send(err)
+        })
+
+    }
+
+    static destroyMovie(req,res){
+        const id = req.params.id
+
+        console.log(id)
+        Movie.destroy({ where: { id: id } })
+        .then(() => {
+            res.redirect('/mv')
+        })
+        .catch(err => {
+            res.send(err)
+        })
+      
+    }
+
+
 }
 
 module.exports = Controller
--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,21 @@ const mvController = require('../controllers/mv.js');
 const phController = require('../controllers/ph.js');
 
 route.get('/',(req,res)=>{
-    res.send('ini home')
+    res.render('home')
 })
 
 route.get('/mv',mvController.findAll)
-// route.get('mv/:id/edit/',mvController.editForm)
-// route.post('mv/:id/edit/',mvController.editMovie)
-// route.get('mv/:id/delete/',mvController.destroyMovie)
-// route.get('mv/add',mvController.createMovie)
-// route.post('mv/add',mvController.addMovie)
+route.get('/mv/:id/edit/',mvController.editForm)
+route.post('/mv/:id/edit/',mvController.changeMovie)
+route.get('/mv/:id/delete/',mvController.destroyMovie)
+route.get('/mv/add',mvController.createMovie)
+route.post('/mv/add',mvController.addMovie)
 
 route.get('/ph',phController.findAll)
-// route.get('ph/:id/edit/',phController.editForm)
-// route.post('ph/:id/edit/',phController.editMovie)
-// route.get('ph/:id/delete/',phController.destroyMovie)
-// route.get('ph/add',phController.createMovie)
-// route.post('ph/add',phController.addMovie)
+// route.get('/ph/:id/edit/',phController.editForm)
+// route.post('/ph/:id/edit/',phController.editMovie)
+// route.get('/ph/:id/delete/',phController.destroyMovie)
+// route.get('/ph/add',phController.createMovie)
+// route.post('/ph/add',phController.addMovie)
 
 module.exports=route

527f9d7f5204aadc861e329a85c8172de63e3fcd
unknown <sonymartha4@gmail.com>
2020-04-20 22:45:57 +0700

    r3

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ const express = require('express')
 const router = express.Router()
 const HomeController = require('../controllers/HomeController')
 const ProductionHouseController = require('../controllers/ProductionHouseController')
+const MovieController = require('../controllers/MovieController')
 
 router.get('/', HomeController.home)
 router.get('/productionhouses', ProductionHouseController.show)
+router.get('/movies', MovieController.show)
+router.get('/movies/addmovieform', MovieController.addMovieForm)
+router.post('/movies/addmovieform', MovieController.addMovie)
+router.get('/movies/:id?/edit', MovieController.editMovieForm)
+// router.post('/movies/addmovieform', MovieController.addMovie)
+router.get('/movies/:id?/delete', MovieController.delete)
 
 module.exports = router

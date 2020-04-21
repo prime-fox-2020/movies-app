6f172e7b3f4a1ab96ac8a8bbc5e4b9f732fe5aa1
fadhlanfm <fafadhlanriz@gmail.com>
2020-04-22 00:18:52 +0700

    Release 7: OK. Tapi masih bingung buat gunain isKabisat dari helpers buat dimasukkin ke validate. Sama masih bingung antara gunain hook atau validate

--- models/movie.js
+++ models/movie.js
@@ -1,4 +1,6 @@
 'use strict';
+// const isKabisat = require('../helpers/isKabisat')
+
 module.exports = (sequelize, DataTypes) => {
   const Sequelize = sequelize.Sequelize
   const Model = Sequelize.Model
@@ -7,11 +9,26 @@ module.exports = (sequelize, DataTypes) => {
 
   Movie.init({
     name: DataTypes.STRING,
-    released_year: DataTypes.INTEGER,
+    released_year: {
+      type: DataTypes.INTEGER,
+      validate: {
+        isKabisat(year){
+          if((year % 4 === 0 && year % 100 !== 0) || (year % 4 === 0 && year % 100 === 0 && year % 400 === 0)){
+              throw new Error('Jangan tahun kabisat')
+          }
+        }
+      }
+    },
     genre: DataTypes.STRING,
     ProductionHouseId: DataTypes.INTEGER,
     rating: DataTypes.INTEGER,
   }, { sequelize })
+
+  // Movie.beforeCreate((movie, options) => {
+  //   if(!isKabisat(this.released_year)){
+  //     throw new Error('Jangan tahun kabisat')
+  //   }
+  // })
 
   Movie.associate = function(models) {
     // associations can be defined here

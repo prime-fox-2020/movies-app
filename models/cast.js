4fa4040cab16ad5164ee53aef8fc7c600f7aaba4
fadhlanfm <fafadhlanriz@gmail.com>
2020-04-21 22:14:10 +0700

    Release 4

--- models/cast.js
+++ models/cast.js
@@ -3,7 +3,12 @@ module.exports = (sequelize, DataTypes) => {
   const Sequelize = sequelize.Sequelize
   const Model = Sequelize.Model
 
-  class Cast extends Model {}
+  class Cast extends Model {
+    getFullName() {
+      return this.first_name + ' ' + this.last_name
+    }
+  }
+
   Cast.init({
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
@@ -11,6 +16,12 @@ module.exports = (sequelize, DataTypes) => {
     birth_year: DataTypes.INTEGER,
     gender: DataTypes.STRING
   }, { sequelize })
+
+  Cast.addHook('beforeCreate', (cast, options) => {
+    if(!cast.last_name){
+      cast.last_name = cast.first_name
+    }
+  });
   
   Cast.associate = function(models) {
     // associations can be defined here
